1ac29d5dd24a1b1e5398214959bb538949e90ae5
viburplus <douglas.lewis@gmail.com>
2025-11-18 16:10:06 -0600

    file read

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,3 +1,23 @@
+import os
+
+MAX_CHARS = 10000
+
 def get_file_content(working_directory, file_path):
 
+    file_contents = ""
 
+    target = os.path.abspath(os.path.join(working_directory, file_path))
+    abs_work = os.path.abspath(working_directory)
+
+    if not (target == abs_work or target.startswith(abs_work + os.sep)):
+        print(f'Error: Cannot read "{file_path}" as it is outside the permitted working directory\n')
+    elif not os.path.isfile(target):
+        print(f'Error: File not found or is not a regular file: "{file_path}"')
+    else:
+        with open(target, "r") as f:
+            file_contents = f.read(MAX_CHARS)
+
+        if os.path.getsize(target) > MAX_CHARS:
+            file_contents += f'[...File "{file_path}" truncated at 10000 characters]'
+
+    return file_contents
--- tests.py
+++ tests.py
@@ -1,10 +1,11 @@
-from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 import os
 
-print(get_files_info("calculator", "."))
 
-print(get_files_info("calculator", "pkg"))
+print(get_file_content("calculator", "main.py"))
 
-print(get_files_info("calculator", "/bin"))
+print(get_file_content("calculator", "pkg/calculator.py"))
 
-print(get_files_info("calculator", "../"))
+print(get_file_content("calculator", "/bin/cat"))
+
+print(get_file_content("calculator", "pkg/does_not_exist.py"))
