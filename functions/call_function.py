88cba9826a458f0ddc6d058639c6e54a69a9b160
viburplus <douglas.lewis@gmail.com>
2025-11-21 17:05:56 -0600

    completed function calling

--- functions/call_function.py
+++ functions/call_function.py
@@ -3,8 +3,7 @@ from functions.get_files_info import get_files_info, schema_get_files_info
 from functions.get_file_content import get_file_content, schema_get_file_content
 from functions.run_python_file import run_python_file, schema_run_python_file
 from functions.write_file import write_file, schema_write_file
-
-WORKING_DIR = "./calculator"
+from config import WORKING_DIR
 
 FUNCTIONS = {
     "get_files_info": get_files_info,
@@ -13,39 +12,48 @@ FUNCTIONS = {
     "run_python_file": run_python_file,
 }
 
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+        schema_get_file_content,
+        schema_run_python_file,
+        schema_write_file,
+    ]
+)
+
 def call_function(function_call_part, verbose=False):
-
-    args = dict(function_call_part.args)
-
+    if verbose:
+        print(
+            f" - Calling function: {function_call_part.name}({function_call_part.args})"
+        )
+    else:
+        print(f" - Calling function: {function_call_part.name}")
+    function_map = {
+        "get_files_info": get_files_info,
+        "get_file_content": get_file_content,
+        "run_python_file": run_python_file,
+        "write_file": write_file,
+    }
     function_name = function_call_part.name
-    func = FUNCTIONS.get(function_name)
-
-    if func is None:
+    if function_name not in function_map:
         return types.Content(
             role="tool",
             parts=[
                 types.Part.from_function_response(
                     name=function_name,
                     response={"error": f"Unknown function: {function_name}"},
-                ),
-             ],
-        )
-    else:
-        args["working_directory"] = WORKING_DIR
-
-        if verbose:
-            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
-        else:
-            print(f" - Calling function: {function_call_part.name}")
-
-        function_result = func(**args)
-
-        return types.Content(
-            role="tool",
-            parts=[
-                types.Part.from_function_response(
-                    name=function_name,
-                    response={"result": function_result},
-                ),
+                )
             ],
         )
+    args = dict(function_call_part.args)
+    args["working_directory"] = WORKING_DIR
+    function_result = function_map[function_name](**args)
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=function_name,
+                response={"result": function_result},
+            )
+        ],
+    )
